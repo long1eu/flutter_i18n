a379d1a1ac15146ab54f12dfc85fb79fc9009056
Razvan Cristian Lung <lung.razvan@gmail.com>
2018-01-18 13:42:33 +0200

    - use the PsiTree to add the resource

--- src/eu/long1/flutter/i18n/actions/ExtractStringResourceArb.kt
+++ src/eu/long1/flutter/i18n/actions/ExtractStringResourceArb.kt
@@ -2,20 +2,21 @@ package eu.long1.flutter.i18n.actions
 
 import com.intellij.codeInsight.intention.HighPriorityAction
 import com.intellij.codeInsight.intention.PsiElementBaseIntentionAction
-import com.intellij.json.psi.JsonFile
+import com.intellij.json.psi.JsonElementGenerator
+import com.intellij.json.psi.JsonObject
 import com.intellij.json.psi.JsonProperty
+import com.intellij.json.psi.JsonPsiUtil
 import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.application.runWriteAction
-import com.intellij.openapi.command.CommandProcessor
+import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.module.ModuleUtilCore
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiManager
-import com.intellij.psi.codeStyle.CodeStyleManager
 import com.intellij.psi.util.PsiTreeUtil
 import eu.long1.flutter.i18n.arb.ArbFileType
+import eu.long1.flutter.i18n.files.FileHelpers
 import org.jetbrains.android.uipreview.CreateArbResourcePanel
 import org.jetbrains.android.uipreview.DialogWrapper
 
@@ -40,10 +41,7 @@ class ExtractStringResourceArb : PsiElementBaseIntentionAction(), HighPriorityAc
         val module = ModuleUtilCore.findModuleForFile(file, project)!!
         val parent = PsiTreeUtil.getParentOfType(element, JsonProperty::class.java) ?: return
 
-        val resDir = module.project.baseDir.findChild("res") ?:
-                module.project.baseDir.createChildDirectory(this, "res")
-        val valuesDir = resDir.findChild("values") ?:
-                resDir.createChildDirectory(this, "values")
+        val valuesDir = FileHelpers.getValuesFolder(project)
 
         ApplicationManager.getApplication().invokeLater {
             val panel = CreateArbResourcePanel(module, parent.name, parent.value?.text?.drop(1)?.dropLast(1) ?: "", valuesDir)
@@ -51,22 +49,14 @@ class ExtractStringResourceArb : PsiElementBaseIntentionAction(), HighPriorityAc
             dialog.title = text
             dialog.showAndGet()
 
+            val property = JsonElementGenerator(project).createProperty(panel.resId, "\"${panel.resValue}\"")
             if (dialog.isOK) {
-                panel.selected.forEach {
-                    val langFile = (psiManager.findFile(valuesDir.findChild(it)!!) as JsonFile?)!!
-                    val jsonProperties = PsiTreeUtil.findChildrenOfAnyType(langFile, JsonProperty::class.java)
-                    val exists = jsonProperties.any { it.name == panel.resId }
-
-                    if (!exists) {
-                        val buffer = StringBuilder(langFile.text.subSequence(0, langFile.textLength - 1))
-                        if (jsonProperties.isNotEmpty()) buffer.append(",")
-                        buffer.append("  \"${panel.resId}\": \"${panel.resValue}\"").append("}")
-                        runWriteAction {
-                            CommandProcessor.getInstance().executeCommand(project, {
-                                documentManager.getDocument(langFile)!!.setText(buffer.toString())
-                                CodeStyleManager.getInstance(psiManager).reformatText(langFile, 0, buffer.length)
-                            }, "Override string in other languages", "Override string in other languages")
-                        }
+                WriteCommandAction.runWriteCommandAction(project) {
+                    panel.selected.forEach {
+                        val langFile = psiManager.findFile(valuesDir.findChild(it)!!)
+                        val json = PsiTreeUtil.getChildOfType(langFile, JsonObject::class.java)!!
+                        if (json.findProperty(panel.resId) == null)
+                            JsonPsiUtil.addProperty(json, property.copy() as JsonProperty, false)
                     }
                 }
             }
