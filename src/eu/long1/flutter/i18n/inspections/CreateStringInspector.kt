11d9a747a45c64ea84a9036df488f0b5d07a2e51
Razvan Cristian Lung <lung.razvan@gmail.com>
2018-05-27 16:13:57 +0300

    check arb files for invalid keys

--- src/eu/long1/flutter/i18n/inspections/CreateStringInspector.kt
+++ src/eu/long1/flutter/i18n/inspections/CreateStringInspector.kt
@@ -6,29 +6,34 @@ import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.codeInspection.ex.BaseLocalInspectionTool
 import com.intellij.psi.PsiFile
 import com.intellij.psi.util.PsiTreeUtil
+import com.jetbrains.lang.dart.DartFileType
 import com.jetbrains.lang.dart.analyzer.DartAnalysisServerService
 import com.jetbrains.lang.dart.psi.DartReferenceExpression
 import eu.long1.flutter.i18n.inspections.quickfix.CreateStringResourceQuickFix
 
 class CreateStringInspector : BaseLocalInspectionTool() {
     override fun checkFile(file: PsiFile, manager: InspectionManager, isOnTheFly: Boolean): Array<ProblemDescriptor>? {
-        val errorList = DartAnalysisServerService.getInstance(file.project).getErrors(file.virtualFile)
-        val errors = errorList.filter { it.code == "undefined_getter" && it.message.contains("defined for the class 'S'.") }
-        if (errors.isEmpty()) return null
+        if (file.fileType == DartFileType.INSTANCE) {
+            val errorList = DartAnalysisServerService.getInstance(file.project).getErrors(file.virtualFile)
+            val errors = errorList.filter { it.code == "undefined_getter" && it.message.contains("defined for the class 'S'.") }
+            if (errors.isEmpty()) return null
 
-        return errors.map {
-            val string = PsiTreeUtil.findElementOfClassAtOffset(file, it.offset, DartReferenceExpression::class.java, false)!!
-            val element = string.parent
+            return errors.map {
+                val string = PsiTreeUtil.findElementOfClassAtOffset(file, it.offset, DartReferenceExpression::class.java, false)!!
+                val element = string.parent
 
-            val quickFix = CreateStringResourceQuickFix(element, string.text)
-            manager.createProblemDescriptor(
-                    element,
-                    quickFix.text,
-                    isOnTheFly,
-                    arrayOf(quickFix),
-                    ProblemHighlightType.GENERIC_ERROR
-            )
-        }.toTypedArray()
+                val quickFix = CreateStringResourceQuickFix(element, string.text)
+                manager.createProblemDescriptor(
+                        element,
+                        quickFix.text,
+                        isOnTheFly,
+                        arrayOf(quickFix),
+                        ProblemHighlightType.GENERIC_ERROR
+                )
+            }.toTypedArray()
+        }
+
+        return null
     }
 
     override fun getDisplayName(): String = "Create string resource from S class reference"
--- src/eu/long1/flutter/i18n/workers/VirtualFileEvent.kt
+++ src/eu/long1/flutter/i18n/workers/VirtualFileEvent.kt
@@ -1,11 +1,25 @@
+@file:Suppress("UNCHECKED_CAST")
+
 package eu.long1.flutter.i18n.workers
 
+import com.intellij.openapi.application.runWriteAction
+import com.intellij.openapi.command.undo.GlobalUndoableAction
+import com.intellij.openapi.command.undo.UndoManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFileCopyEvent
 import com.intellij.openapi.vfs.VirtualFileEvent
 import com.intellij.openapi.vfs.VirtualFileListener
 import com.intellij.openapi.vfs.VirtualFileMoveEvent
+import com.intellij.psi.PsiDocumentManager
+import com.intellij.psi.PsiManager
 import eu.long1.flutter.i18n.files.FileHelpers
+import org.yaml.snakeyaml.DumperOptions
+import org.yaml.snakeyaml.Yaml
+import org.yaml.snakeyaml.constructor.SafeConstructor
+import org.yaml.snakeyaml.nodes.Tag
+import org.yaml.snakeyaml.representer.Representer
+import org.yaml.snakeyaml.resolver.Resolver
+import java.io.File
 
 /**
  * Supported files:
@@ -24,13 +38,61 @@ import eu.long1.flutter.i18n.files.FileHelpers
  * */
 
 class VirtualFileEvent(private val project: Project) : VirtualFileListener {
+
     private val resFolder = FileHelpers.getResourceFolder(project)
+    private val undoManager = UndoManager.getInstance(project)!!
+    private val documentManager = PsiDocumentManager.getInstance(project)!!
+
+    private fun yaml(): Yaml = Yaml(SafeConstructor(), Representer(), DumperOptions(), object : Resolver() {
+        override fun addImplicitResolvers() {
+            this.addImplicitResolver(Tag.BOOL, Resolver.BOOL, "yYnNtTfFoO")
+            this.addImplicitResolver(Tag.NULL, Resolver.NULL, "~nN\u0000")
+            this.addImplicitResolver(Tag.NULL, Resolver.EMPTY, null as String?)
+            this.addImplicitResolver(Tag("tag:yaml.org,2002:value"), Resolver.VALUE, "=")
+            this.addImplicitResolver(Tag.MERGE, Resolver.MERGE, "<")
+        }
+    })
 
     override fun fileCreated(event: VirtualFileEvent) {
         if (!isRes(event.file.path)) return
+        if (!acceptedExtensions.contains(event.file.extension)) return
 
+        // check to see if this a different resolution for a file we already added
+        if (event.parent!!.name.matches(Regex("\\d.\\dx"))) {
+            if (event.parent!!.findChild(event.fileName) != null) {
+                println("Base file exists. We don't do anything in this case.")
+            } else {
+                println("Base file doesn't exists. Should we consider this file as base? Not for now, so we do nothing.")
+            }
+            return
+        }
+
+
+        val originalText = File(project.basePath + "/pubspec.yaml").readText()
+        val pubspecInfo = loadPubspecInfo(originalText)
+        val flutterValues = pubspecInfo!!["flutter"] as HashMap<String, Any>
+        println(flutterValues)
+        val assets: ArrayList<String> = flutterValues["assets"] as? ArrayList<String> ?: arrayListOf()
+
+        assets.add(event.fileName)
+        flutterValues["assets"] = assets
+
+        val data = yaml().dumpAsMap(pubspecInfo)
+
+        println(data)
         println("created")
         println(event.file.path)
+
+        runWriteAction {
+            val document = documentManager.getDocument(PsiManager.getInstance(project).findFile(project.baseDir.findChild("pubspec.yaml")!!)!!)!!
+            document.setText(data)
+            documentManager.commitDocument(document)
+
+            undoManager.undoableActionPerformed(object : GlobalUndoableAction(event.file) {
+                override fun redo() {}
+                override fun undo() {}
+            })
+        }
     }
 
     override fun fileDeleted(event: VirtualFileEvent) {
@@ -61,4 +123,12 @@ class VirtualFileEvent(private val project: Project) : VirtualFileListener {
     }
 
     private fun isRes(path: String): Boolean = path.contains(resFolder.path)
+
+    private fun loadPubspecInfo(yamlContents: String): Map<String, Any>? {
+        return yaml().load(yamlContents) as Map<String, Any>
+    }
+
+    companion object {
+        val acceptedExtensions = arrayListOf("json", "jpeg", "jpg", "jpe", "jif", "jfif", "jfi", "webp", "gif", "png", "bmp", "dib", "wbmp")
+    }
 }
