c8a26c6cdc28c12b94a280617d3838f09847c9bb
Razvan Cristian Lung <lung.razvan@gmail.com>
2018-11-18 16:31:21 +0200

    remove old project files

--- src/eu/long1/flutter/i18n/arb/ArbFileTypeFactory.kt
+++
@@ -1,9 +0,0 @@
-package eu.long1.flutter.i18n.arb
-
-import com.intellij.json.JsonFileTypeFactory
-import com.intellij.openapi.fileTypes.FileTypeConsumer
-
-class ArbFileTypeFactory : JsonFileTypeFactory() {
-    override fun createFileTypes(consumer: FileTypeConsumer) =
-            consumer.consume(ArbFileType, ArbFileType.defaultExtension)
-}
--- src/eu/long1/flutter/i18n/items/MethodItem.kt
+++
@@ -1,9 +0,0 @@
-package eu.long1.flutter.i18n.items
-
-import com.jetbrains.lang.dart.psi.DartComponent
-
-data class MethodItem(val method: String?, val dartMethod: DartComponent?) {
-    companion object {
-        val empty = MethodItem(null, null)
-    }
-}
