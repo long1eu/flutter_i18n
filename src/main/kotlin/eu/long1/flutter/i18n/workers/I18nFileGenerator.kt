99c6d2064c398b6bb36df2eb600e89193fa5d92f
Noor Dawod <noor@fineswap.com>
2018-12-18 19:31:57 +0100

    Adds support for matching locales with the same language, but with different country codes.

--- src/main/kotlin/eu/long1/flutter/i18n/workers/I18nFileGenerator.kt
+++ src/main/kotlin/eu/long1/flutter/i18n/workers/I18nFileGenerator.kt
@@ -368,24 +368,24 @@ import 'package:flutter/material.dart';
             """    ];
   }
 
-  LocaleListResolutionCallback listResolution({Locale fallback}) {
+  LocaleListResolutionCallback listResolution({Locale fallback, bool withCountry = true}) {
     return (List<Locale> locales, Iterable<Locale> supported) {
       if (locales == null || locales.isEmpty) {
         return fallback ?? supported.first;
       } else {
-        return _resolve(locales.first, fallback, supported);
+        return _resolve(locales.first, fallback, supported, withCountry);
       }
     };
   }
 
-  LocaleResolutionCallback resolution({Locale fallback}) {
+  LocaleResolutionCallback resolution({Locale fallback, bool withCountry = true}) {
     return (Locale locale, Iterable<Locale> supported) {
-      return _resolve(locale, fallback, supported);
+      return _resolve(locale, fallback, supported, withCountry);
     };
   }
 
-  Locale _resolve(Locale locale, Locale fallback, Iterable<Locale> supported) {
-    if (locale == null || !isSupported(locale)) {
+  Locale _resolve(Locale locale, Locale fallback, Iterable<Locale> supported, bool withCountry) {
+    if (locale == null || !isSupported(locale, withCountry)) {
       return fallback ?? supported.first;
     }
 
@@ -416,8 +416,27 @@ import 'package:flutter/material.dart';
   }
 
   @override
-  bool isSupported(Locale locale) =>
-    locale != null && supportedLocales.contains(locale);
+  bool isSupported(Locale locale, bool withCountry) {
+    if (locale != null) {
+      for (Locale supportedLocale in supportedLocales) {
+        // Language must always match both locales.
+        if (supportedLocale.languageCode != locale.languageCode) {
+          continue;
+        }
+
+        // If country code matches, return this locale.
+        if (supportedLocale.countryCode == locale.countryCode) {
+          return true;
+        }
+
+        // If no country requirement is requested, check if this locale has no country.
+        if (true != withCountry && (supportedLocale.countryCode == null || supportedLocale.countryCode.isEmpty)) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
 
   @override
   bool shouldReload(GeneratedLocalizationsDelegate old) => false;
